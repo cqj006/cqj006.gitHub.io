4d9d64d712f9dc751ec6c98cec53b9d8a844a003
‘cqj006’ <1303748498@qq.com>
2023-05-25 17:34:15 +0800

    改3

--- data.js
+++ data.js
@@ -8,20 +8,12 @@ var list = [
     'https://dev.yqcloud.top/?ref=',
     'https://1chat.cc/?ref=',
     'https://chat.lingdong5.com/?ref=',
-    'https://ai.hxkj.vip/?ref=',
-    'https://chatgptmirror.com/?ref=',
-    'https://chatbot.theb.ai/?ref=',
-    'https://www.promptboom.com/?ref=',
-    'https://chat.gptplus.one/?ref=',
-    'https://smart-chat.xiami.one/?ref=',
-    'https://chat19.xeasy.me/?ref=',
-    'https://xc.com/?ref=',
-    'https://freegpt.one/?ref=',
-    'https://chatgpt-cn.co/?ref='
+    'https://ai.hxkj.vip/?ref='
 ]
-var reg=/\/\/(.*?)\//;
-var l = list.map((item) => {
+var reg = /\/\/(.*?)\//;
+var l = list.map((item, index) => {
     return {
+        id: index,
         title: reg.exec(item)[1].trim(),
         icon: 'el-icon-s-tools',
         url: item,
